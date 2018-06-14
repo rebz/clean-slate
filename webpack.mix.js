17e4707bd454e3ff04769cdeef1d2a5e33b9b502
Christopher Johnson <esreborn@gmail.com>
2018-06-14 17:24:43 -0400

    - added .env-example  - updated mix config  - update package.json

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,35 +1,47 @@
+require('dotenv').config({
+    path: __dirname + '/.env'
+});
+
+let env = process.env
+
 const mix = require('laravel-mix')
 
 mix
     .js('resources/js/app.js', 'public/js/app.js')
     .sass('resources/sass/app.scss', 'public/css/app.css')
     .autoload({
+        axios: "axios",
         vue: "Vue",
         lodash: "_"
     })
     .extract([
         "lodash",
-        "nprogress",
         "vue",
         "vue-router"
     ])
-    .sourceMaps()
-    .browserSync({
-        open: 'external',
-        host: "drive.dev",
-        proxy: "drive.dev",
-        files: [
-            "public/**/*.html",
-            "public/js/**/*.js",
-            "public/css/**/*.css"
-        ]
-    })
     .webpackConfig({
         resolve : {
             alias : {
-                '@js.views' : path.join(__dirname, 'resources/js/views'),
-                '@js.components' : path.join(__dirname, 'resources/js/components'),
-                '@js.helpers' : path.join(__dirname, 'resources/js/mixins/helpers')
+                '@js.views' : path.join(__dirname, 'resources/assets/js/views'),
+                '@js.components' : path.join(__dirname, 'resources/assets/js/components'),
+                '@js.helpers' : path.join(__dirname, 'resources/assets/js/mixins/helpers')
             }
         }
     })
+
+if (mix.inProduction()) {
+    mix.version()
+    mix.disableNotifications()
+} else {
+    mix.sourceMaps()
+        .browserSync({
+            open: 'external',
+            host: env.APP_DOMAIN,
+            proxy: env.APP_DOMAIN,
+            files: [
+                "public/**/*.html",
+                "public/js/**/*.js",
+                "public/css/**/*.css"
+            ]
+        })
+}
