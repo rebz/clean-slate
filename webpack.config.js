995e5083ab7621edf41e65713e2dc7d2d17a25b4
Christopher Johnson <esreborn@gmail.com>
2018-07-24 21:58:53 -0400

    Using webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+
 // https://github.com/jantimon/html-webpack-plugin
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
@@ -8,4 +9,23 @@ module.exports = {
             template: "./resources/html/index.html"
         }),
     ],
+
+    devServer: {
+        // Display only errors to reduce the amount of output.
+        stats: "errors-only",
+
+        historyApiFallback: true, // TODO - read more about this
+        overlay: true, // TODO - https://www.npmjs.com/package/error-overlay-webpack-plugin
+
+        // Parse host and port from env to allow customization.
+        //
+        // If you use Docker, Vagrant or Cloud9, set
+        // host: options.host || "0.0.0.0";
+        //
+        // 0.0.0.0 is available to all network devices
+        // unlike default `localhost`.
+        host: 'cleanslate.test', // Defaults to `localhost`
+        port: 8080, // Defaults to 8080
+        open: true, // Open the page in browser
+    },
 };
